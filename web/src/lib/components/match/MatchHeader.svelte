b1bdcda8d4cb298f155dd313bff2c605f72f7654
Tom Law <lawhowang@icloud.com>
2025-10-03 00:35:42 +0800

    feat: enhance MatchHeader styling for improved readability and consistency

--- web/src/lib/components/match/MatchHeader.svelte
+++ web/src/lib/components/match/MatchHeader.svelte
@@ -11,7 +11,7 @@
 	class="sticky left-0 right-0 top-0 flex min-h-20 w-full flex-col items-center justify-center border-b border-b-[1px] border-b-gray-300 bg-white text-center"
 >
 	<div class="flex w-[400px] flex-col items-center justify-center">
-		<h2>
+		<h2 class="text-lg font-semibold text-gray-700">
 			{#if matchInfo?.gameOver}
 				Game Over
 			{:else}
@@ -25,29 +25,29 @@
 		</h2>
 		<div class="mt-2 flex w-full items-center justify-between">
 			<div
-				class="w-1/3 overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-2 text-center"
+				class="w-1/3 overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-2 text-center text-gray-800"
 				class:bg-blue-200={matchInfo?.myTurn ==
 					(matchInfo?.player1.nickname === playerInfo.nickname)}
 				class:bg-gray-200={matchInfo?.myTurn !=
 					(matchInfo?.player1.nickname === playerInfo.nickname)}
 			>
-				<span class="font-semibold">{matchInfo?.player1.nickname}</span>
+				<span class="text-sm font-bold">{matchInfo?.player1.nickname}</span>
 			</div>
 			{#if matchInfo?.deadline}
 				<div>
-					<span class="ml-4 text-sm text-gray-500">
+					<span class="ml-4 text-gray-500">
 						<Countdown deadline={matchInfo?.deadline} />
 					</span>
 				</div>
 			{/if}
 			<div
-				class="w-1/3 overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-2 text-center"
+				class="w-1/3 overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-2 text-center text-gray-800"
 				class:bg-blue-200={matchInfo?.myTurn ==
 					(matchInfo?.player2.nickname === playerInfo.nickname)}
 				class:bg-gray-200={matchInfo?.myTurn !=
 					(matchInfo?.player2.nickname === playerInfo.nickname)}
 			>
-				<span class="font-semibold">{matchInfo?.player2.nickname}</span>
+				<span class="text-sm font-bold">{matchInfo?.player2.nickname}</span>
 			</div>
 		</div>
 	</div>
